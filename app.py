2f4c8a32a1f670ef7c0b29492645b5a51fdb9a56
aditi6shukla-ui <aditi6shukla@gmail.com>
2026-01-21 20:05:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ st.title("Universal Facts App")
 
 word = st.text_input("Type any planet, animal, fruit, car brand, or country")
 
-key = word.strip()
-
-if key:
+if word:
     try:
-        summary = wikipedia.summary(key, sentences=2, auto_suggest=False)
-        st.write(summary)
+        result = wikipedia.summary(word, sentences=2)
+        st.write(result)
     except wikipedia.exceptions.DisambiguationError as e:
         st.write(wikipedia.summary(e.options[0], sentences=2))
     except wikipedia.exceptions.PageError:
